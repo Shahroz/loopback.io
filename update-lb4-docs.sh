ad69fa27ed6d887a9353b167317da6218101431a
biniam <biniam@ca.ibm.com>
2018-03-09 15:16:41 -0500

    fixup! re-add org

--- update-lb4-docs.sh
+++ update-lb4-docs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+org=strongloop
 repo=loopback-next
 target=pages/en/lb4
 branch=move-docs-into-monorepo
@@ -9,6 +11,7 @@ mkdir -p $tmp
 # given the GitHub org, repo, and branch
 curl -L https://github.com/$org/$repo/archive/$branch.tar.gz \
    | tar --strip-components 1 -C $tmp -xzf- '*.md'
+
 cp -R $tmp/packages $target/readmes/$repo
 #rename relative link extensions from md to html
 sed -E -i '' 's/\(([^(]+)\.md/(\1.html/g' $tmp/docs/*.md
